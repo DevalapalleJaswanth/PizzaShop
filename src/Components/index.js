fee2d392bddbea46327a7bb53cb91301efb306eb
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-04-22 04:04:02 +0530

    1 file modified, 1 added

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+//import Banner from './Banner.js';
+import DropDown from './DropDown';
+import Card from './Card';
+//import Search from './Search';
+import AddBlock from './AddBlock';
+export { DropDown, Card, AddBlock };
--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Banner, Card, Search } from '../Components';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
 export default function HomePage() {
   return <div>Home Page</div>;
 }
