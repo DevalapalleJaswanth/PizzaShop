5eb0c93dd2baabb7626e82e6dfc164da3a13f2b9
DevalapalleJaswanth <jaswanthreddy040@gmail.com>
2022-04-22 02:21:46 +0530

    4 files modified

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 export default function DropDown({ label, list, state, setState }) {
+  const [showDropDown, setShowDropDown] = useState(false);
   return (
     <div>
       <div>
         <div className="label">{label && label} : </div>
         <div className="value">{state && state}</div>
-        <div></div>
+        <div onClick={() => setShowDropDown(true)}>
+          {!showDropDown ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+        </div>
       </div>
-      <div className="list">
+      <div
+        className="list"
+        style={{ display: showDropDown ? 'block' : 'none' }}
+      >
         {list &&
           list.map((item, i) => {
             <div key={i} className="item">
